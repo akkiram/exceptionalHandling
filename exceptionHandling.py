ab285edd06904759902c4d8ba37fe36d4ef7116a
akkiram <114799941+akkiram@users.noreply.github.com>
2022-12-24 13:24:33 +0530

    try

---
+++ exceptionHandling.py
@@ -0,0 +1,23 @@
+# try:
+    # this block of code will run and throw errors if there are any
+
+# except:
+    # this will raise the errors
+
+# else:
+    # this will execute if there are no errors
+
+# finally:
+    # this will execute regardless the result of try and except
+
+
+try:
+    f = open("demo.py")
+    try:
+        f.write("ABC")
+    except:
+        print("Error in file")
+    finally:
+        f.close()
+except:
+    print("File not exist")
